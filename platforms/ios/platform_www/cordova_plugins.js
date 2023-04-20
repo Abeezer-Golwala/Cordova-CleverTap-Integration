78d2d766a5a90b423668b4e7e868551a93a0fd91
abeezer <abeezergolwala98@gmail.com>
2023-04-20 18:03:43 +0530

    Webview commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "clevertap-cordova.CleverTap",
+      "file": "plugins/clevertap-cordova/www/CleverTap.js",
+      "pluginId": "clevertap-cordova",
+      "clobbers": [
+        "CleverTap"
+      ]
+    },
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "clevertap-cordova": "2.4.0",
+    "cordova-plugin-inappbrowser": "5.0.0"
+  };
+});
---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,81 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+document.getElementById("btn").addEventListener("click",onPush);
+document.getElementById("inbox").addEventListener("click",onInboxCordova);
+document.getElementById("native").addEventListener("click",onNativeCordova);
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    CleverTap.setDebugLevel(3);
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+    CleverTap.onUserLogin ({
+                'Name': 'Abeezer',
+    //          'Identity': '9766609',
+                "Phone no.": "+91977994121",
+                'Email': 'abeezer@cordova.com'
+            });
+    CleverTap.createNotificationChannel("abtest", "abtest", "Test Channel", 5, true);
+    CleverTap.initializeInbox();
+    var ref = cordova.InAppBrowser.open('https://abeezerwebtest.000webhostapp.com/cordovawebview.html', '_blank', 'location=yes');
+//    window.open = cordova.InAppBrowser.open;
+//    ref.show();
+    ref.addEventListener('message', messageCallBack);
+
+}
+function messageCallBack(params){
+console.log("webview"+JSON.stringify(params));
+var clevertapdata = JSON.parse(JSON.stringify(params.data));
+  console.log("test"+clevertapdata.Type);
+  console.log("test"+JSON.stringify(clevertapdata.Type));
+  //checking the type of the data recieved from webview if its an event, onuserlogin or profile set data.
+    if (JSON.stringify(clevertapdata.Type) == "\"event\""){
+      console.log("test"+clevertapdata.Type);
+      CleverTap.recordEventWithName(clevertapdata.EventName,clevertapdata.Payload);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.Payload));
+    }
+    else if (JSON.stringify(clevertapdata.Type) == "\"onuserlogin\""){
+      CleverTap.onUserLogin(clevertapdata.Payload);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.Payload));
+    }
+    else if (JSON.stringify(clevertapdata.Type) == "\"profileset\""){
+      CleverTap.profileSet(clevertapdata.Payload);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.Payload));
+    }
+    else if (JSON.stringify(clevertapdata.Type) == "\"chargedevent\""){
+      CleverTap.recordChargedEvent(clevertapdata.chargedetails, clevertapdata.items);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.chargedetails)+"\n"+JSON.stringify(clevertapdata.items));
+    }
+    else if (JSON.stringify(clevertapdata.Type) == "\"profileSetMultiValuesForKey\""){
+      CleverTap.profileSetMultiValuesForKey(clevertapdata.values, clevertapdata.key);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.key)+"\n"+JSON.stringify(clevertapdata.values));
+    }
+    else if (JSON.stringify(clevertapdata.Type) == "\"profileRemoveMultiValueForKey\""){
+      CleverTap.profileRemoveMultiValueForKey(clevertapdata.value, clevertapdata.key);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.key)+"\n"+JSON.stringify(clevertapdata.value));
+    }
+    else if (JSON.stringify(clevertapdata.Type) == "\"profileAddMultiValueForKey\""){
+      CleverTap.profileAddMultiValueForKey(clevertapdata.value, clevertapdata.key);
+      console.log(JSON.stringify(clevertapdata.Type)+"\n"+JSON.stringify(clevertapdata.key)+"\n"+JSON.stringify(clevertapdata.value));
+    }
+
+}
+function onPush(){
+    CleverTap.recordEventWithName("AbeezerPushEvent");
+}
+function onInboxCordova()
+{
+    CleverTap.recordEventWithName("Abeezergetmsg");
+    CleverTap.showInbox({});
+    CleverTap.createNotification()
+}
+
+
+function onNativeCordova()
+{
+    CleverTap.recordEventWithName("abeezernativedisp");
+    CleverTap.getAllDisplayUnits(function(val) {
+    document.getElementById("nativemsg").innerHTML =JSON.stringify(val[0].content[0].message.text).replace(/['"]+/g, '');
+    document.getElementById("nativetitle").innerHTML =JSON.stringify(val[0].content[0].title.text).replace(/['"]+/g, '');
+    console.log("Native Display units are "+JSON.stringify(val[0].content[0].message.text));
+    });
+}
+
